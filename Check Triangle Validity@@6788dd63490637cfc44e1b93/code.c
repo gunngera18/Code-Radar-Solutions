ec93c3ad9505ad99f10369a760b546f73808f037
gunngera18 <gulatiamol90@gmail.com>
2025-03-17 19:47:20 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,7 +3,7 @@ int main()
 {
     int a,b,c;
     printf("enter the first side : ");
-    scanf("%d",&a)
+    scanf("%d",&a);
      printf("enter the second side : ");
      scanf("%d",&b);
       printf("enter the third side : ");
