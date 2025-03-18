21545e6fd3df82562ea2c8f073e96a3dac7e0151
gunngera18 <gulatiamol90@gmail.com>
2025-03-18 23:42:34 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(z == +) printf("%d",x+y);
+    else if(z == -) printf("%d",x-y);
+    else if(z == *) printf("%d",x*y);
+    else if(z == /) printf("%d",x/y);
+    else printf("error");
+}
